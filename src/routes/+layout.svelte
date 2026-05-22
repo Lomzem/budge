edb99284763a6ca940569d795d096998d72f7a1d
Lawjay Lee <lawjaylee1@icloud.com>
2026-05-22 11:47:25 -0700

    init: Hook up Convex in layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,11 @@
   import "./layout.css"
   import favicon from "$lib/assets/favicon.svg"
 
+  import { PUBLIC_CONVEX_URL } from "$env/static/public"
+  import { setupConvex } from "convex-svelte"
+
   let { children } = $props()
+  setupConvex(PUBLIC_CONVEX_URL)
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
